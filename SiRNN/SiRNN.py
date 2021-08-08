7be30612c129812d2a40c6267673aa1dd39d1fec
nkmayo <nkmayo@gmail.com>
2021-08-07 22:08:58 -0400

    Combined cells, duplicated others for easy running

--- SiRNN/SiRNN.py
+++ SiRNN/SiRNN.py
@@ -1,5 +1,6 @@
-"""Something is off as the SiRNN doesn't learn, even with a 'tanh' activation which is essentially
-just a regular RNN. Weight initialization can't be the 'sinusoid'?
+"""
+Something is off as the SiRNN doesn't learn, even with a 'tanh' activation which is essentially
+just a regular RNN. Weight initialization can't be a sinusoid?
 Sources: 
 https://pytorch.org/tutorials/intermediate/char_rnn_classification_tutorial.html
 https://www.youtube.com/watch?v=SEnXr6v2ifU
@@ -87,14 +88,15 @@ class SiRNN(nn.Module):
 # Let's keep it simple to start and just have a single SiRNN followed by a dense layer and softmax output
 class SiRNNet(nn.Module):
     def __init__(self, input_features, recurrent_size, intermediate_size, output_features, 
-                 outermost_linear=True, first_omega_0=30, intermediate_omega_0=30.):
+                 outermost_linear=True, first_omega_0=30, intermediate_omega_0=30., activation='sin'):
         super().__init__()
         self.recurrent_size = recurrent_size
         self.SiRNN = SiRNN(input_features, recurrent_size, intermediate_size,
-                                  is_first=True, omega_0=first_omega_0)
+                                  is_first=True, omega_0=first_omega_0, activation=activation)
         self.final_linear = nn.Linear(intermediate_size, output_features)
         self.softmax = nn.LogSoftmax(dim=1)
 
+        self.init_weights()
     
     def forward(self, input, hidden):
         # input = input.clone().detach().requires_grad_(True) # allows to take derivative w.r.t. input
@@ -105,7 +107,10 @@ class SiRNNet(nn.Module):
 
     def zero_recurrent(self, batch_size):
         """Returns initial recurrent state: a tensor of zeros to initially input into the recurrent layer"""
-        return torch.zeros(batch_size, self.recurrent_size) # pointless function?   
+        return torch.zeros(batch_size, self.recurrent_size) # pointless function?
+
+    def init_weights(self):
+        self.SiRNN.init_weights() 
 
 # %% Check to make sure dummy data works on both SiRNN and a SiRNNet
 # model_S = SiRNN(50, 20, 10)
@@ -203,7 +208,7 @@ class SiRNNet2(nn.Module):
         output = self.softmax(output) # added softmax
         return output, coords        
 
-# %%
+# %% With SiRNN
 from __future__ import unicode_literals, print_function, division
 from io import open
 import glob
@@ -245,7 +250,7 @@ for filename in findFiles('data/names/*.txt'):
     category_lines[category] = lines
 
 n_categories = len(all_categories)
-# %%
+
 def letterToIndex(letter):
     return all_letters.find(letter)
 
@@ -267,33 +272,9 @@ print(letterToTensor('J'))
 
 print(lineToTensor('Jones').size())
 
-n_hidden=128
-rnn = SiRNN(n_letters, n_hidden, n_categories, activation='tanh')
+n_hidden=64
+rnn = SiRNNet(n_letters, n_hidden, n_categories, n_categories, activation='tanh')
 
-# %% The tutorials RNN that seems to work...
-class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(RNN, self).__init__()
-
-        self.hidden_size = hidden_size
-
-        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
-        self.i2o = nn.Linear(input_size + hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
-
-    def forward(self, input, hidden):
-        combined = torch.cat((input, hidden), 1)
-        hidden = self.i2h(combined)
-        output = self.i2o(combined)
-        output = self.softmax(output)
-        return output, hidden
-
-    def initHidden(self):
-        return torch.zeros(1, self.hidden_size)
-
-n_hidden = 128
-rnn = RNN(n_letters, n_hidden, n_categories)
-# %%
 input = lineToTensor('Albert')
 hidden = torch.zeros(1, n_hidden)
 
@@ -322,11 +303,12 @@ def randomTrainingExample():
 for i in range(10):
     category, line, category_tensor, line_tensor = randomTrainingExample()
     print('category =', category, '/ line =', line)
-# %% Something is off, it doesn't converge even when using 'tanh' activation
+
+# Something is off, it doesn't converge even when using 'tanh' activation
 # or is it just the initial conditions with the sin activation?
 criterion = nn.NLLLoss()
 
-learning_rate = 0.005 # If you set this too high, it might explode. If too low, it might not learn
+learning_rate = 0.05 # If you set this too high, it might explode. If too low, it might not learn
 
 def train(category_tensor, line_tensor):
     # for the SiRNN
@@ -391,7 +373,76 @@ import matplotlib.ticker as ticker
 
 plt.figure()
 plt.plot(all_losses)
-# %% A 'conventional' RNN (though only with a softmax activation...)
+
+# %% Via Tutorial
+from __future__ import unicode_literals, print_function, division
+from io import open
+import glob
+import os
+
+def findFiles(path): return glob.glob(path)
+
+print(findFiles('data/names/*.txt'))
+
+import unicodedata
+import string
+
+all_letters = string.ascii_letters + " .,;'"
+n_letters = len(all_letters)
+
+# Turn a Unicode string to plain ASCII, thanks to https://stackoverflow.com/a/518232/2809427
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+        and c in all_letters
+    )
+
+print(unicodeToAscii('Ślusàrski'))
+
+# Build the category_lines dictionary, a list of names per language
+category_lines = {}
+all_categories = []
+
+# Read a file and split into lines
+def readLines(filename):
+    lines = open(filename, encoding='utf-8').read().strip().split('\n')
+    return [unicodeToAscii(line) for line in lines]
+
+for filename in findFiles('data/names/*.txt'):
+    category = os.path.splitext(os.path.basename(filename))[0]
+    all_categories.append(category)
+    lines = readLines(filename)
+    category_lines[category] = lines
+
+n_categories = len(all_categories)
+
+import torch
+
+# Find letter index from all_letters, e.g. "a" = 0
+def letterToIndex(letter):
+    return all_letters.find(letter)
+
+# Just for demonstration, turn a letter into a <1 x n_letters> Tensor
+def letterToTensor(letter):
+    tensor = torch.zeros(1, n_letters)
+    tensor[0][letterToIndex(letter)] = 1
+    return tensor
+
+# Turn a line into a <line_length x 1 x n_letters>,
+# or an array of one-hot letter vectors
+def lineToTensor(line):
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for li, letter in enumerate(line):
+        tensor[li][0][letterToIndex(letter)] = 1
+    return tensor
+
+print(letterToTensor('J'))
+
+print(lineToTensor('Jones').size())
+
+import torch.nn as nn
+
 class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(RNN, self).__init__()
@@ -415,6 +466,99 @@ class RNN(nn.Module):
 n_hidden = 128
 rnn = RNN(n_letters, n_hidden, n_categories)
 
+input = lineToTensor('Albert')
+hidden = torch.zeros(1, n_hidden)
+
+output, next_hidden = rnn(input[0], hidden)
+print(output)
+
+def categoryFromOutput(output):
+    top_n, top_i = output.topk(1)
+    category_i = top_i[0].item()
+    return all_categories[category_i], category_i
+
+print(categoryFromOutput(output))
+
+import random
+
+def randomChoice(l):
+    return l[random.randint(0, len(l) - 1)]
+
+def randomTrainingExample():
+    category = randomChoice(all_categories)
+    line = randomChoice(category_lines[category])
+    category_tensor = torch.tensor([all_categories.index(category)], dtype=torch.long)
+    line_tensor = lineToTensor(line)
+    return category, line, category_tensor, line_tensor
+
+for i in range(10):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    print('category =', category, '/ line =', line)
+
+# Something is off, it doesn't converge even when using 'tanh' activation
+# or is it just the initial conditions with the sin activation?
+criterion = nn.NLLLoss()
+
+learning_rate = 0.005 # If you set this too high, it might explode. If too low, it might not learn
+
+def train(category_tensor, line_tensor):
+    hidden = rnn.initHidden()
+
+    rnn.zero_grad()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    loss = criterion(output, category_tensor)
+    loss.backward()
+
+    # Add parameters' gradients to their values, multiplied by learning rate
+    for p in rnn.parameters():
+        p.data.add_(p.grad.data, alpha=-learning_rate)
+
+    return output, loss.item()
+
+import time
+import math
+
+n_iters = 100000
+print_every = 5000
+plot_every = 1000
+
+# Keep track of losses for plotting
+current_loss = 0
+all_losses = []
+
+def timeSince(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+start = time.time()
+
+for iter in range(1, n_iters + 1):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    output, loss = train(category_tensor, line_tensor)
+    current_loss += loss
+
+    # Print iter number, loss, name and guess
+    if iter % print_every == 0:
+        guess, guess_i = categoryFromOutput(output)
+        correct = '✓' if guess == category else '✗ (%s)' % category
+        print('%d %d%% (%s) %.4f %s / %s %s' % (iter, iter / n_iters * 100, timeSince(start), loss, line, guess, correct))
+
+    # Add current loss avg to list of losses
+    if iter % plot_every == 0:
+        all_losses.append(current_loss / plot_every)
+        current_loss = 0
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+plt.figure()
+plt.plot(all_losses)
 # %%
 # Keep track of correct guesses in a confusion matrix
 confusion = torch.zeros(n_categories, n_categories)
