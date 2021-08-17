d79da8709897ab618662b5da5a0f305d6d770208
nkmayo <nkmayo@gmail.com>
2021-08-16 22:09:48 -0400

    add scikit-learn example for printing

--- NeuralNetCircle.py
+++ NeuralNetCircle.py
@@ -116,4 +116,68 @@ sns.histplot(data=data,
              )
 plt.show()
 
+# %% Aha! it's in scikit-learn examples
+# https://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html#sphx-glr-auto-examples-classification-plot-classifier-comparison-py
+from matplotlib.colors import ListedColormap
+
+h = .2  # step size in the mesh
+
+x_min, x_max = min(X_val[:,0].min(), X_test[:,0].min()) - 1, max(X_val[:,0].max(), X_test[:,0].max()) + 1
+y_min, y_max = min(X_val[:,1].min(), X_test[:,1].min()) - 1, max(X_val[:,1].max(), X_test[:,1].max()) + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), 
+                     np.arange(y_min, y_max, h))
+
+# just plot the dataset first
+cm = plt.cm.RdBu
+cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+ax = plt.subplot(1, 1, 1) # not sure what dpi does exactly, why not 72?
+ax.set_title("Input data")
+# Plot the training points
+ax.scatter(predictors[0::50,0], predictors[0::50,1], c=target[0::50], cmap=cm_bright,
+            edgecolors='k') # only display every 50th, no need for 10k points
+# Plot the testing points
+ax.scatter(X_test[:,0], X_test[:,1], c=y_test, cmap=cm_bright, alpha=0.6,
+            edgecolors='k')
+ax.set_xlim(xx.min(), xx.max())
+ax.set_ylim(yy.min(), yy.max())
+ax.set_xticks(())
+ax.set_yticks(())
+
+grid = np.stack([xx.ravel(), yy.ravel()], axis=1)
+
+# probs = target
+# probs.reshape(xx.shape)
+# ax.contourf(xx, yy, probs, cmap=cm, alpha=0.8)
+# ax.set_title("Prediction contours")
+"""
+ax = plt.subplot(1, 1, 1)
+
+# Plot the decision boundary. For that, we will assign a color to each
+# point in the mesh [x_min, x_max]x[y_min, y_max].
+if hasattr(clf, "decision_function"):
+    Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+else:
+    Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+
+# Plot the training points
+ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
+            edgecolors='k')
+# Plot the testing points
+ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
+            edgecolors='k', alpha=0.6)
+
+ax.set_xlim(xx.min(), xx.max())
+ax.set_ylim(yy.min(), yy.max())
+ax.set_xticks(())
+ax.set_yticks(())
+if ds_cnt == 0:
+    ax.set_title(name)
+ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
+        size=15, horizontalalignment='right')
+i += 1
+"""
 # %%
