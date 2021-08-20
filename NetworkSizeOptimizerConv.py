5d683d53e5128f25270c135eb33d6d2ccfa59079
nkmayo <nkmayo@gmail.com>
2021-08-19 22:16:40 -0400

    Enabled batch size >1, added conv version

--- NetworkSizeOptimizerConv.py
+++ NetworkSizeOptimizerConv.py
@@ -11,11 +11,6 @@ thereby providing more assurance that the minimum converged to is the global min
 a local minimum. Furthermore, the hope is that by starting small and growing the network you
 end up saving time by not training unnecessarily large networks.
 Let's use a 'simple' feed forward network and the MNIST dataset as an example to practice on...
-
-Sources:
-https://theaisummer.com/Autoencoder/
-https://github.com/deep-learning-with-pytorch/dlwpt-code
-https://github.com/jankrepl/mildlyoverfitted/tree/master/github_adventures/mixup
 """
 # %%
 import torch
@@ -44,6 +39,13 @@ class myAutoenc(nn.Module):
         Flattened input dimension size.
     n_hidden : int
         Number of hidden layers.
+    input : nn.Conv2d
+        Initial 2D convolutional layer
+    flatten : nn.Flatten
+        Layer to flatten the convolutional output
+    lin1 : nn.Linear
+        Linear layer that connects the flattened convolutional output
+        with the subsequent hidden layers
     hidden_layers : nn.ModuleList
         List of hidden layers that are each composed of a `Linear`
         and `LeakyReLU` module.
@@ -53,21 +55,23 @@ class myAutoenc(nn.Module):
 
     """
 
-    def __init__(self, n_features=28*28, hidden_dims=(4,4)):
+    def __init__(self, n_features=28*28, hidden_dims=(4,4), convChan=5):
         super().__init__()
         self.n_input = n_features
         self.n_hidden = len(hidden_dims)
-        dims = (n_features,) + hidden_dims
+        self.input = nn.Conv2d(1, convChan, kernel_size=3, padding=1)
+        self.flatten = nn.Flatten()
+        self.lin1 = nn.Linear(n_features*convChan, hidden_dims[0])
         self.hidden_layers = nn.ModuleList(
             [
                 nn.Sequential(
-                    nn.Linear(dims[i], dims[i + 1]),
-                    nn.Sigmoid(),
+                    nn.Linear(hidden_dims[i], hidden_dims[i + 1]),
+                    nn.LeakyReLU(0.2),
                 )
-                for i in range(self.n_hidden)
+                for i in range(self.n_hidden-1)
             ]
         )
-        self.output = nn.Linear(dims[-1], n_features) # output same as input size
+        self.output = nn.Linear(hidden_dims[-1], n_features) # output same as input size
 
     def forward(self, x):
         """Run forward pass.
@@ -76,6 +80,9 @@ class myAutoenc(nn.Module):
         x : torch.Tensor
             Input of shape `(1, n_features)`.
         """
+        x = self.input(x)
+        x = self.flatten(x)
+        x = self.lin1(x)
 
         for module in self.hidden_layers:
             x = module(x)
@@ -91,8 +98,8 @@ def training_loop(n_epochs, optimizer, model, loss_fn, train_loader):
 
         for imgs, _ in train_loader:
             imgs = imgs.to(device=device)
-            outputs = model(imgs.view(-1,1,784))
-            loss = loss_fn(outputs, imgs.view(-1,1,784))
+            outputs = model(imgs.view(-1,1,28,28))
+            loss = loss_fn(outputs, imgs.view(-1,784))
 
             optimizer.zero_grad()
             loss.backward()
@@ -147,13 +154,13 @@ plt.imshow(inimg.permute(1,2,0)) # have to switch from CxHxW to HxWxC before plo
 
 # %%
 with torch.no_grad(): # don't want the gradients being updated just to view an output
-    outimg = model(inimg.view(1,-1).unsqueeze(0)).view(-1,28,28)
+    outimg = model(inimg.unsqueeze(0)).view(-1,28,28)
 
 plt.imshow(outimg.permute(1,2,0))
 # %% Before training example (random output)
 untrained = myAutoenc()
 with torch.no_grad(): # don't want the gradients being updated just to view an output
-    untrainedOut = untrained(inimg.view(1,-1).unsqueeze(0)).view(-1,28,28)
+    untrainedOut = untrained(inimg.unsqueeze(0)).view(-1,28,28)
 
 plt.imshow(untrainedOut.permute(1,2,0))
 # %%
